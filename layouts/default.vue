e0de36f17abe82519c19ee29a0108c04ea300c5e
Mateus Gomes <mateus.correia@assert.ifpb.edu.br>
2024-04-12 17:34:37 -0300

    add buttons

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,60 @@
 <template>
-    <div>
+  <v-layout>
+    <v-app-bar color="primary" scroll-behavior="elevate" prominent>
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title>My files</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon="mdi-magnify" variant="text"></v-btn>
+
+      <v-btn icon="mdi-filter" variant="text"></v-btn>
+
+      <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          subtitle="sandra_a88@gmailcom"
+          title="Sandra Adams"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item
+          prepend-icon="mdi-folder"
+          title="My Files"
+          value="myfiles"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account-multiple"
+          title="Shared with me"
+          value="shared"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-star"
+          title="Starred"
+          value="starred"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid>
         <slot />
-    </div>
+      </v-container>
+    </v-main>
+  </v-layout>
 </template>
 
 <script setup lang="ts">
-
+const drawer = ref(false);
 </script>
-
-<style scoped>
-
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,43 @@
 <template>
   <v-card class="mx-auto" prepend-icon="mdi-home" width="400">
     <template v-slot:title> This is a title </template>
-    <v-card-text> This is content </v-card-text>
+    <v-card-text class="d-flex flex-column ga-2">
+      <v-btn
+        v-for="button in buttons"
+        :key="button.color"
+        :color="button.color"
+      >
+        Button
+      </v-btn>
+    </v-card-text>
   </v-card>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<script setup lang="ts">
+const buttons = ref([
+  {
+    color: "primary",
+  },
+  {
+    color: "secondary",
+  },
+  {
+    color: "success",
+  },
+  {
+    color: "info",
+  },
+  {
+    color: "warning",
+  },
+  {
+    color: "error",
+  },
+  {
+    color: "dark",
+  },
+  {
+    color: "light",
+  },
+]);
+</script>
