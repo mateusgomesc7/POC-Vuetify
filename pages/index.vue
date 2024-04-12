72844a48b2ed1fbe265192d2e7c6feeac255365a
Mateus Gomes <mateus.correia@assert.ifpb.edu.br>
2024-04-12 13:50:47 -0300

    git commit -m "first commit"

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-card class="mx-auto" prepend-icon="mdi-home" width="400">
+    <template v-slot:title> This is a title </template>
+    <v-card-text> This is content </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
